7413d9b1bd17ebdc955d8211c591ae7b7d73af77
Probal Ghosh <probalghoshofficial@gmail.com>
2025-07-04 00:51:46 +0530

    added axios

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,35 @@
-// import { useState } from 'react'
-
+import { useEffect, useState } from 'react'
+import axios from "axios"
 import './App.css'
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [jokes, setJokes] = useState([])
+  useEffect(()=>{
+    axios.get('https://localhost:3000/jokes')
+    .then((response)=>{
+      setJokes(response.data)
+    })
+    .catch((error)=>{
+      console.log(error)
+    })
+  })
 
   return (
     <>
-      frontend & backend
-    </>
+      <h1>frontend & backend</h1>
+      <p>JOKES: {jokes.length}</p>
+    
+  
+
+  {
+    jokes.map((joke)=>{
+      <div key={joke.id}>
+      <h3>{joke.title}</h3>
+      <p>{joke.content}</p>
+      </div>
+    })
+  }
+  </>
   )
 }
 
