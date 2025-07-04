26902ded097db3394e2e109d58f3d0ae6166e943
Probal Ghosh <probalghoshofficial@gmail.com>
2025-07-04 13:15:31 +0530

    connected with backend using proxy

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,14 +5,15 @@ import './App.css'
 function App() {
   const [jokes, setJokes] = useState([])
   useEffect(()=>{
-    axios.get('https://localhost:3000/jokes')
+    axios.get('/api/jokes')
     .then((response)=>{
+      console.log(response.data)
       setJokes(response.data)
     })
     .catch((error)=>{
       console.log(error)
     })
-  })
+  }, [])
 
   return (
     <>
@@ -22,12 +23,12 @@ function App() {
   
 
   {
-    jokes.map((joke)=>{
+    jokes.map((joke)=>(
       <div key={joke.id}>
       <h3>{joke.title}</h3>
       <p>{joke.content}</p>
       </div>
-    })
+    ))
   }
   </>
   )
